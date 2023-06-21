208a74cacccd617dd71b01c124ab77f872560e42
Rahul Yerme <rahul.yerme@nitorinfotech.com>
2023-06-21 15:02:11 +0000

    Updated server.js

--- app/server.js
+++ app/server.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 8080
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello... This is deployment of node.js application to AKS using azure pipelines.')
 })
 
 app.listen(port, () => {
